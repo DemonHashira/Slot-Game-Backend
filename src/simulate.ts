4940bd3e5455420e43f03dd1cfbbb6688327278c
DemonHashira <v.logodazhki@outlook.com>
2024-04-11 10:12:32 +0300

    Added simple simulation script

---
+++ src/simulate.ts
@@ -0,0 +1,30 @@
+import { Slots } from "./models/slots";
+import { gameConfig } from "./types/configuration";
+
+export function simulateSpins(spins: number) {
+  const slotMachine = new Slots(gameConfig);
+  let totalBet = 0;
+  let totalPayout = 0;
+
+  console.time("Simulation execution time");
+
+  for (let i = 0; i < spins; i++) {
+    const betAmount = 1;
+    totalBet += betAmount;
+
+    const payout = slotMachine.spin();
+    if (payout !== undefined) {
+      totalPayout += payout;
+    }
+  }
+
+  console.timeEnd("Simulation execution time");
+
+  const rtp = (totalPayout / totalBet) * 100;
+  console.log(`Total spins: ${spins}`);
+  console.log(`Total bet: ${totalBet}`);
+  console.log(`Total payout: ${totalPayout}`);
+  console.log(`RTP: ${rtp.toFixed(2)}%`);
+}
+
+simulateSpins(1000000);
