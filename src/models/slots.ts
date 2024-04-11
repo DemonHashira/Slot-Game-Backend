eaaae06c3a727982067aa3b4900b841c198977a5
DemonHashira <v.logodazhki@outlook.com>
2024-04-12 02:04:22 +0300

    Added additional comments

--- src/models/slots.ts
+++ src/models/slots.ts
@@ -15,6 +15,7 @@ export class Slots {
   public spin(): { spinResult: Symbol[][]; payout: number } {
     let spinResult: Symbol[][] = [];
 
+    // Spinning each reel and getting the symbols
     for (let i = 0; i < this.reels.length; i++) {
       let reelSpin: Symbol[] = [];
       for (let j = 0; j < this.config.rowsCount; j++) {
@@ -25,6 +26,7 @@ export class Slots {
 
     let payout = this.calculatePayout(spinResult);
 
+    // Printing the result of the spin
     console.log("Spin result:");
     spinResult[0].forEach((_, i) => {
       console.log(spinResult.map((reel) => reel[i]).join(" | "));
@@ -39,6 +41,7 @@ export class Slots {
   private calculatePayout(spinResult: Symbol[][]): number {
     let totalPayout = 0;
 
+    // Helper function to find matching symbols in a line
     const findMatches = (symbols: Symbol[], fromLeft: boolean = true) => {
       let matchCount = 1;
       let symbolToMatch = symbols[fromLeft ? 0 : symbols.length - 1];
@@ -53,15 +56,19 @@ export class Slots {
       return { matchCount, symbol: symbolToMatch };
     };
 
+    // Iterating over each line and calculating the payout
     for (const line of this.config.lines) {
       const symbolsInLine = line.pattern.map(
         (rowIndex, reelIndex) => spinResult[reelIndex][rowIndex]
       );
+
+      // Checking for matches from left and right
       const leftMatch = findMatches(symbolsInLine, true);
       const rightMatch = findMatches(symbolsInLine, false);
       const bestMatch =
         leftMatch.matchCount > rightMatch.matchCount ? leftMatch : rightMatch;
 
+      // If we have at least 3 matching symbols and the symbol exists in the config we calculate the payout
       if (bestMatch.matchCount >= 3 && this.config.symbols[bestMatch.symbol]) {
         const payoutForSymbol =
           this.config.symbols[bestMatch.symbol][bestMatch.matchCount - 1];
